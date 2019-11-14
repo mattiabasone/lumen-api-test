c5d0b0f9cf85fe4976522d587447c7e2f6438db1
Mattia Basone <mattia.basone@gmail.com>
2019-11-14 19:06:03 +0100

    adds basic docker setup (wip)

---
+++ docker/nginx-php/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Adding host machine hostname
+sudo bash -c 'echo -e "\n# Hostname for gateway" >> /etc/hosts'
+sudo bash -c "echo -e \"`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal\n\" >> /etc/hosts"
+
+cd /app
+
+composer install --no-progress --no-ansi --no-interaction
+
+sudo /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 ## Lumen API Test
 
+### Build and start docker environment
+
+- `cd docker/`
+- `cp .env.example .env`
+- `docker-compose build`
+- `docker-compose up`
